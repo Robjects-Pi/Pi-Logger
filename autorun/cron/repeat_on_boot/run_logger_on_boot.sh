1a8c2661f60f6b8474578ea91fd61fad1888f45b
Mike <garzarobm@gmail.com>
2024-09-08 15:45:55 -0500

    Refactor cron scripts to add continuous logging functionality

--- autorun/cron/repeat_on_boot/run_logger_on_boot.sh
+++ autorun/cron/repeat_on_boot/run_logger_on_boot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOGGER_SCRIPT_DIR="/home/pi/raspberry_pi_logger"
-
+INTERVAL=5  # Interval in seconds
 
 if [ ! -f "$LOGGER_SCRIPT_DIR/logger.py" ]; then
     echo "Error: $LOGGER_SCRIPT_DIR/logger.py not found."
@@ -10,4 +10,5 @@ fi
 
 while true; do
     python3 "$LOGGER_SCRIPT_DIR/logger.py"
+    sleep $INTERVAL
 done
