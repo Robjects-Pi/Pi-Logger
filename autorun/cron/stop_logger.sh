cda242d860cac9fe7debe298a8259b01494d0bd8
Mike <garzarobm@gmail.com>
2024-09-08 15:01:40 -0500

    Refactor cron README and scripts

--- autorun/cron/stop_logger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOGGER_SCRIPT="/home/pi/raspberry_pi_logger/run_log.sh"
-LOG_FILE="/home/pi/raspberry_pi_logger/cron_logger.log"
-
-# Kill the process
-pkill -f "$LOGGER_SCRIPT"
-
-echo "Logger stopped at $(date)" >> $LOG_FILE
